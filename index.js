d2fd0e2ee103e6ddf8c26ff56f20ddae73f9c898
Teshome Kurabachew <dear.teshe@gmail.com>
2022-03-03 11:44:02 +0300

    Fix ESlint error

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const projectModal = document.querySelector('.project-modal');
 const container = document.querySelector('.container');
 const projButton = document.querySelectorAll('.btn-pro');
 const toggleMenu = document.querySelector('.toggle');
+// const closeIcon = document.querySelector('icon-close');
 
 /// ///////////////////////////////// Mobile Menu
 toggleMenu.addEventListener('click', () => {
@@ -13,86 +14,21 @@ toggleMenu.addEventListener('click', () => {
   body.classList.toggle('overflow-hidden');
 });
 
-const toggleModal = () => {
+function closeModal() {
   projectModal.classList.toggle('show-modal');
   container.classList.toggle('hide-container');
-};
-
-/// ///////////////////////////////// Data manipulation in popup window
-
-for (let i = 0; i < projButton.length; i++) {
-  projButton[i].addEventListener('click', () => {
-    container.classList.toggle('hide-container');
-    projectModal.classList.toggle('show-modal');
-
-    const modalSection2 = `
-    <div class="modal-section">
-      <div class="icon-close" onclick="toggleModal()">
-        <svg class="icon">
-          <use xlink:href="images/sprite.svg#ic_cross"></use>
-        </svg>
-      </div>
-      <div class="modal-heading">
-        <h2 class="project-name-detail">${projects[i].name}</h2>
-        <ul class="languages">           
-          ${projects[i].technologies.map((e) => `<li>${e}</li>`).join('')}
-        </ul>
-      </div>
-      <div class="proj-img">
-        <img class="proj-img--main" src="${
-  projects[i].picture[0]
-}" alt="Project Detail Picture" />
-        <div class="proj-img--list">
-          ${projects[i].picture
-    .map(
-      (e) => `<img class="proj-img proj-img_sub" src="${e}" alt="Project Detail Picture" />`,
-    )
-    .join('')}
-        </div>
-      </div>
-      <div class="project-description">
-        ${projects[i].description.map((e) => `<p>${e}</p>`)}
-      </div>
-      <div class="project-link">
-        <a href="${projects[i].linkToLive}" class="btn btn-light btn-live">
-          <span class="btn-name">See Live</span>
-          <span class="btn-icon">
-            <svg class="icon">
-              <use xlink:href="images/sprite.svg#ic_link"></use>
-            </svg>
-          </span>
-        </a>
-        <a href=""${projects[i].linkToSource}" class="btn btn-light btn-source">
-          <span class="btn-name">See Source</span>
-          <span class="btn-icon">
-            <svg class="icon">
-              <use xlink:href="images/sprite.svg#ic_github_white"></use>
-            </svg>
-          </span>
-        </a>
-      </div>
-      <div class="page-link">
-        <a href="" class="prev-page" onclick="prev()"><span class="left-arrow">&larr;</span>
-          <span class="page-text">Previous project</span></a>
-        <a href="" class="next-page" onclick="next()">
-          <span class="page-text">Next project</span><span class="right-arrow">&rarr;</span></a>
-      </div>
-    </div>
-    `;
-    projectModal.innerHTML = modalSection2;
-  });
 }
 
 /// ///////////////////////////////// Data store for popup window
-let projects = [
+const projects = [
   {
     name: 'Project name goes here',
     technologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
     picture: [
-      'images/project/Rectangle21(1).png',
-      'images/project/Rectangle21(1).png',
-      'images/project/Rectangle21(1).png',
-      'images/project/Rectangle21(1).png',
+      'images/project/Rectangle21.png',
+      'images/project/Rectangle21.png',
+      'images/project/Rectangle21.png',
+      'images/project/Rectangle21.png',
     ],
     description: [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
@@ -105,10 +41,10 @@ let projects = [
     name: 'Project name goes here',
     technologies: ['HTML/CSS', 'Ruby on Rails', 'JavaScript'],
     picture: [
-      'images/project/Rectangle21.png',
-      'images/project/Rectangle21.png',
-      'images/project/Rectangle21.png',
-      'images/project/Rectangle21.png',
+      'images/project/Rectangle21(1).png',
+      'images/project/Rectangle21(1).png',
+      'images/project/Rectangle21(1).png',
+      'images/project/Rectangle21(1).png',
     ],
     description: [
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi',
@@ -182,3 +118,67 @@ let projects = [
     linkToSource: '#',
   },
 ];
+
+/// ///////////////////////////////// Data manipulation in popup window
+
+for (let i = 0; i < projects.length; i += 1) {
+  projButton[i].addEventListener('click', () => {
+    container.classList.toggle('hide-container');
+    projectModal.classList.toggle('show-modal');
+    const modalSection2 = `
+    <div class="modal-section">
+      <div class="icon-close" ${closeModal} onclick="closeModal()">
+        <svg class="icon">
+          <use xlink:href="images/sprite.svg#ic_cross"></use>
+        </svg>
+      </div>
+      <div class="modal-heading">
+        <h2 class="project-name-detail">${projects[i].name}</h2>
+        <ul class="languages">           
+          ${projects[i].technologies.map((e) => `<li>${e}</li>`).join('')}
+        </ul>
+      </div>
+      <div class="proj-img">
+        <img class="proj-img--main" src="${
+  projects[i].picture[0]
+}" alt="Project Detail Picture" />
+        <div class="proj-img--list">
+          ${projects[i].picture
+    .map(
+      (e) => `<img class="proj-img proj-img_sub" src="${e}" alt="Project Detail Picture" />`,
+    )
+    .join('')}
+        </div>
+      </div>
+      <div class="project-description">
+        ${projects[i].description.map((e) => `<p>${e}</p>`)}
+      </div>
+      <div class="project-link">
+        <a href="${projects[i].linkToLive}" class="btn btn-light btn-live">
+          <span class="btn-name">See Live</span>
+          <span class="btn-icon">
+            <svg class="icon">
+              <use xlink:href="images/sprite.svg#ic_link"></use>
+            </svg>
+          </span>
+        </a>
+        <a href=""${projects[i].linkToSource}" class="btn btn-light btn-source">
+          <span class="btn-name">See Source</span>
+          <span class="btn-icon">
+            <svg class="icon">
+              <use xlink:href="images/sprite.svg#ic_github_white"></use>
+            </svg>
+          </span>
+        </a>
+      </div>
+      <div class="page-link">
+        <a href="" class="prev-page" onclick="prev()"><span class="left-arrow">&larr;</span>
+          <span class="page-text">Previous project</span></a>
+        <a href="" class="next-page" onclick="next()">
+          <span class="page-text">Next project</span><span class="right-arrow">&rarr;</span></a>
+      </div>
+    </div>
+    `;
+    projectModal.innerHTML = modalSection2;
+  });
+}
